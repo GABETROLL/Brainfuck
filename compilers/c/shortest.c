892433e1e52234f328731045a048158c25893b79
GABETROLL <gabriel7icplhappy@gmail.com>
2022-12-21 19:07:26 -0400

    Remove unecessary parenthesis

--- compilers/c/shortest.c
+++ compilers/c/shortest.c
@@ -2,21 +2,21 @@
 
 int main(int argc, char **argv)
 {
-	for (; *(argv[1]) && *(argv[1]) != ']'; argv[1]++)
+	for (; *argv[1] && *argv[1] != ']'; argv[1]++)
 	{
-		if (*(argv[1]) == 43)
+		if (*argv[1] == 43)
 			puts("(*p)++;");
-		if (*(argv[1]) == 45)
+		if (*argv[1] == 45)
 			puts("(*p)--;");
-		if (*(argv[1]) == 62)
+		if (*argv[1] == 62)
 			puts("p++;");
-		if (*(argv[1]) == 60)
+		if (*argv[1] == 60)
 			puts("p--;");
-		if (*(argv[1]) == 46)
+		if (*argv[1] == 46)
 			puts("putchar(*p);");
-		if (*(argv[1]) == 44)
+		if (*argv[1] == 44)
 			puts("getchar();");
-		if (*(argv[1]) == 91)
+		if (*argv[1] == 91)
 		{
 			puts("while(*p){");
 			argv[1]++;
