1460d2ffaddeb21426d9dacff37beb4030b4332e
GABETROLL <gabriel7icplhappy@gmail.com>
2022-12-22 20:28:26 -0400

    Replace char* with if-else

--- compilers/c/shortest.c
+++ compilers/c/shortest.c
@@ -1,2 +1,2 @@
 #include <stdio.h>
-int main(int c,char**v){for(;*v[1]&&*v[1]!=']';v[1]++){char*s=*v[1]==43?"(*p)++;":*v[1]==45?"(*p)--;":*v[1]==62?"p++;":*v[1]==60?"p--;":*v[1]==46?"putchar(*p);":*v[1]==44?"getchar();":"";if(*s)puts(s);else if(*v[1]==91){puts("while(*p){");v[1]++;main(c,v);puts("}");}}return 0;}
+int main(int c,char**v){for(;*v[1]&&*v[1]!=']';v[1]++){if(*v[1]!=91)puts(*v[1]==43?"(*p)++;":*v[1]==45?"(*p)--;":*v[1]==62?"p++;":*v[1]==60?"p--;":*v[1]==46?"putchar(*p);":*v[1]==44?"getchar();":"");else{puts("while(*p){");v[1]++;main(c,v);puts("}");}}return 0;}
