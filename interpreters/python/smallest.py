3a659bfc4418d69fb78560b69459c82136980e11
GABETROLL <gabriel7icplhappy@gmail.com>
2022-12-22 23:31:05 -0400

    Remove indentations from if statements

--- interpreters/python/smallest.py
+++ interpreters/python/smallest.py
@@ -2,25 +2,18 @@ c,d,p=input(),[0]*30,0
 def b(i):
     global p
     while i<len(c):
-        if c[i]=="+":
-            d[p]+=1
-        if c[i]=="-":
-            d[p]-=1
-        if c[i]==">":
-            p+=1
-        if c[i]=="<":
-            p-=1
-        if c[i]==".":
-            print(chr(d[p]),end="")
-        if c[i]==",":
-            d[p]=ord(input())
+        if c[i]=="+":d[p]+=1
+        if c[i]=="-":d[p]-=1
+        if c[i]==">":p+=1
+        if c[i]=="<":p-=1
+        if c[i]==".":print(chr(d[p]),end="")
+        if c[i]==",":d[p]=ord(input())
         if c[i]=="[":
             ni=i
             while d[p]:
                 ni = b(i+1)
             i=ni
-        elif c[i]=="]":
-            return i
+        elif c[i]=="]":return i
         i+=1
     return i
 b(0)
